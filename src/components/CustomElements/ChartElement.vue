450959c5dd7bc6dad20b99f9a48ce44a4ee4666e
Harshit <harshitassist@gmail.com>
2021-01-22 03:20:42 +0530

    Chart Element Created

---
+++ src/components/CustomElements/ChartElement.vue
@@ -0,0 +1,37 @@
+<template>
+  <vs-row vs-justify="center">
+    <vs-col
+      type="flex"
+      vs-justify="center"
+      vs-align="center"
+      vs-lg="12"
+      vs-xs="12"
+      vs-sm="12"
+    >
+      <vs-card>
+        <div slot="header">
+          <h3>
+            {{title}}
+          </h3>
+        </div>
+        <div>
+           
+        
+           
+        </div>
+      </vs-card>
+    </vs-col>
+  </vs-row>
+</template>
+
+<script>
+export default {
+  name: "ChartElement",
+  props: ['title','tabledata'],
+  data: () => ({
+     
+    
+  }),
+  
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
         vs-align="center"
         vs-lg="2"
         vs-xs="12"
-        vs-sm="12"
+        vs-sm="0"
       >
         <vs-card>
           <div slot="header">
@@ -200,16 +200,118 @@
         </vs-card>
       </vs-col>
     </vs-row>
+
+    <!-- section 3 -->
+    <vs-row vs-justify="center" style="margin-top: 10px;">
+      <vs-col
+        class="card"
+        type="flex"
+        vs-justify="center"
+        vs-align="center"
+        vs-lg="5"
+        vs-xs="12"
+        vs-sm="12"
+      >
+        <ChartElement title="Candidates List" />
+      </vs-col>
+      <vs-col
+        class="card"
+        type="flex"
+        vs-justify="center"
+        vs-align="center"
+        vs-lg="5"
+        vs-xs="12"
+        vs-sm="12"
+      >
+        <ChartElement title="Sony A7RIV" />
+      </vs-col>
+    </vs-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ChartElement from "../components/CustomElements/ChartElement.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    ChartElement
+  },
   data() {
-    return {};
+    return {
+      tdata: [
+      {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        website: "hildegard.org"
+      },
+      {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv",
+        website: "anastasia.net"
+      },
+      {
+        id: 3,
+        name: "Clementine Bauch",
+        username: "Samantha",
+        email: "Nathan@yesenia.net",
+        website: "ramiro.info"
+      },
+      {
+        id: 4,
+        name: "Patricia Lebsack",
+        username: "Karianne",
+        email: "Julianne.OConner@kory.org",
+        website: "kale.biz"
+      },
+      {
+        id: 5,
+        name: "Chelsey Dietrich",
+        username: "Kamren",
+        email: "Lucio_Hettinger@annie.ca",
+        website: "demarco.info"
+      },
+      {
+        id: 6,
+        name: "Mrs. Dennis Schulist",
+        username: "Leopoldo_Corkery",
+        email: "Karley_Dach@jasper.info",
+        website: "ola.org"
+      },
+      {
+        id: 7,
+        name: "Kurtis Weissnat",
+        username: "Elwyn.Skiles",
+        email: "Telly.Hoeger@billy.biz",
+        website: "elvis.io"
+      },
+      {
+        id: 8,
+        name: "Nicholas Runolfsdottir V",
+        username: "Maxime_Nienow",
+        email: "Sherwood@rosamond.me",
+        website: "jacynthe.com"
+      },
+      {
+        id: 9,
+        name: "Glenna Reichert",
+        username: "Delphine",
+        email: "Chaim_McDermott@dana.io",
+        website: "conrad.com"
+      },
+      {
+        id: 10,
+        name: "Clementina DuBuque",
+        username: "Moriah.Stanton",
+        email: "Rey.Padberg@karina.biz",
+        website: "ambrose.net"
+      }
+    ]
+    };
   }
 };
 </script>
@@ -222,7 +324,6 @@ export default {
   margin-left: 15px;
   margin-right: 15px;
   margin-top: 5px;
-  
 }
 .cardbutton {
   margin-left: 5px;
