cc57761dfb92c1bbf6d534fb9b8de600c8a138bd
Pallavi Singh <you@example.com>
2021-01-25 03:13:25 +0530

    CICD Test 5

---
+++ public/buildnotifier.sh
@@ -0,0 +1,8 @@
+curl --location --request POST 'https://discord.com/api/webhooks/802511811722346517/6ob7XaIgnU2wLSrJRMLqXNd2L3YByeESOp0Gyh0F62Qwn9vucTVZAzT_M4U30xMxZvzj' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: __cfduid=d750ffa6f71f98de1c6dede55ad2ca1341611404163' \
+--data-raw '{
+        "username": "Assessment UI Server | 192.46.208.114",
+    "avatar_url": "",
+    "content": "Build Successfull"
+}'
