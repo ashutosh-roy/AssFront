2ed31bf77a9665f4898e8555a4ad5cf5464dd723
ROHAN KAR <rohankar1999@gmail.com>
2021-02-26 11:32:26 +0530

    image/long/short/video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,55 @@
 <template>
   <div>
     <b-row align-h="center">
-      <b-button style="float:right; margin-left:90%" v-b-modal.modal-1
+      <b-button
+        style="float:right; margin-left:90%"
+        v-b-modal.modal-1
+        :disabled="formActivity()"
         ><i class="fas fa-plus"></i
       ></b-button>
     </b-row>
     <b-modal
       id="modal-1"
       centered
+      hide-footer
       title="Choose Question Type"
       style="width:90vh"
     >
       <b-button class="buttonchoice" variant="outline-secondary">MCQ</b-button>
-      <b-button class="buttonchoice" variant="outline-secondary"
+      <b-button
+        class="buttonchoice"
+        variant="outline-secondary"
+        @click="
+          longanswer = true;
+          $bvModal.hide('modal-1');
+        "
         >Long Answer</b-button
       >
-      <b-button class="buttonchoice" variant="outline-secondary"
+      <b-button
+        class="buttonchoice"
+        variant="outline-secondary"
+        @click="
+          shortAnswer = true;
+          $bvModal.hide('modal-1');
+        "
         >Short Answer</b-button
       >
-      <b-button class="buttonchoice" variant="outline-secondary"
+      <b-button
+        class="buttonchoice"
+        variant="outline-secondary"
+        @click="
+          image = true;
+          $bvModal.hide('modal-1');
+        "
         >Image</b-button
       >
-      <b-button class="buttonchoice" variant="outline-secondary"
+      <b-button
+        class="buttonchoice"
+        variant="outline-secondary"
+        @click="
+          video = true;
+          $bvModal.hide('modal-1');
+        "
         >Video</b-button
       >
       <b-button class="buttonchoice" variant="outline-secondary"
@@ -36,33 +64,56 @@
       <b-button class="buttonchoice" variant="outline-secondary"
         >Unseen Passage</b-button
       >
-      <template #modal-footer="">
-        <!-- Emulate built in modal footer ok and cancel button actions -->
-        <b-button size="sm" variant="success" @click="ok()" v-show="false">
-          OK
-        </b-button>
-        <b-button size="sm" variant="danger" @click="cancel()" v-show="false">
-          Cancel
-        </b-button>
-        <!-- Button with custom close trigger value -->
-        <b-button
-          size="sm"
-          variant="outline-secondary"
-          @click="hide('forget')"
-          v-show="false"
-        >
-          Forget it
-        </b-button>
-      </template>
     </b-modal>
+    <b-row align-h="center">
+      <b-col cols="7">
+        <multipleTypeQuestions v-show="false" />
+        <LongAnswer v-if="longanswer" v-on:delete-form="longanswer = false" />
+        <ShortAnswer
+          v-if="shortAnswer"
+          v-on:delete-form="shortAnswer = false"
+        />
+        <Image v-if="image" v-on:delete-form="image = false" />
+        <Video v-if="video" v-on:delete-form="video = false" />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import multipleTypeQuestions from "../components/forms/multipleType";
+import LongAnswer from "../components/forms/longAnswer";
+import ShortAnswer from "../components/forms/shortAnswer";
+import Image from "../components/forms/imageQuestion";
+import Video from "../components/forms/videoQuestion";
 
 export default {
   name: "Home",
+  components: {
+    multipleTypeQuestions,
+    LongAnswer,
+    ShortAnswer,
+    Image,
+    Video,
+  },
+  data() {
+    return {
+      shortAnswer: false,
+      longanswer: false,
+      image: false,
+      video: false,
+    };
+  },
+  methods: {
+    formActivity() {
+      if (this.shortAnswer == true || this.longanswer == true) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 };
 </script>
 <style scoped>
