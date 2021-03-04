32146b83ce83fcc4c685940109a52b1a3e28c025
Tousif7851 <mohamed.ta@simplify3x.com>
2021-03-04 12:19:53 +0530

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav"></div>
+    <div id="nav">
+      <Navbar/>
+    </div>
     <router-view />
+    
   </div>
 </template>
-
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components:{Navbar}
+}
+</script>
 <style lang="scss"></style>
